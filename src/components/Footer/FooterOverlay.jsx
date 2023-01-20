8ce3c33035d7777a724452f726abfcda980e1c7e
Joseph Jack <joseph.w.jack@gmail.com>
2023-01-20 15:05:22 -0800

    Finish footer container and Footer components w/ styling

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -5,8 +5,7 @@ import './FooterOverlay.css';
 const FooterOverlay = () => (
   <div className="app__footerOverlay">
     <div className="app__footerOverlay-black" />
-    <div className="app__footerOverlay-image" />
-
+    <div className="app__footerOverlay-image app__bg" />
   </div>
 );
 
